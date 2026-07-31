a6b476dd2a446ffbc30e2291c35870a525c84c6e
Megz-Mugz <rohan.mengle123@gmail.com>
2026-07-31 08:07:20 -0400

    used get_if for variants

--- src/typechecker.cpp
+++ src/typechecker.cpp
@@ -4,40 +4,34 @@
 
 bool Typechecker::typecheck_ast(const AST& ast, Schema& schema){
 
-    if (std::holds_alternative<CreateTableAST>(ast)) {
-        auto create_table_ast = std::get<CreateTableAST>(ast);
+    if (auto* create_table_ast = std::get_if<CreateTableAST>(&ast)) {
 
-        schema.insert_table(create_table_ast);
+        schema.insert_table(*create_table_ast);
 
-        code_gen.lower_create_ast(create_table_ast);
+        code_gen.lower_create_ast(*create_table_ast);
 
         // Set a breakpoint on the next line.
-        std::println("Valid Create Statement");
-    } else if (std::holds_alternative<InsertStatementAST>(ast)) {
-        auto insert_statement_ast = std::get<InsertStatementAST>(ast);
+        // std::println("Valid Create Statement");
+    } else if (auto* insert_statement_ast = std::get_if<InsertStatementAST>(&ast)) {
 
         std::println("Valid Insert Statement");
 
-    } else if (std::holds_alternative<UpdateStatementAST>(ast)) {
-        auto update_statement_ast = std::get<UpdateStatementAST>(ast);
+    } else if (auto* update_statement_ast = std::get_if<UpdateStatementAST>(&ast)) {
 
         std::println("Valid Update Statement");
     
     
-    } else if (std::holds_alternative<SelectStatementAST>(ast)) {
-        auto select_statement_ast = std::get<SelectStatementAST>(ast);
+    } else if (auto* select_statement_ast = std::get_if<SelectStatementAST>(&ast)) {
 
         std::println("Valid Select Statement");
     } 
     
-    else if (std::holds_alternative<DropStatementAST>(ast)) {
-        auto drop_statement_ast = std::get<DropStatementAST>(ast);
+    else if (auto* drop_statement_ast = std::get_if<DropStatementAST>(&ast)) {
 
         std::println("Valid drop Statement");
     }
 
-    else if (std::holds_alternative<DeleteStatementAST>(ast)) {
-        auto delete_statement_ast = std::get<DeleteStatementAST>(ast);
+    else if (auto* delete_statement_ast = std::get_if<DeleteStatementAST>(&ast)) {
 
         std::println("Valid Delete Statement");
     
